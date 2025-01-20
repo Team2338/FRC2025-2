bd2f2082b748d50ad0a384fc80b345bf6c9b83b7
rowan dyche <rowdyche09@gmail.com>
2025-01-20 11:09:56 -0600

    added the base of the code

---
+++ src/main/java/team/gif/robot/UI.java
@@ -0,0 +1,26 @@
+package team.gif.robot;
+
+/**
+ *   Initialize the shuffleboard here.
+ *   Shuffleboard is a modern looking driveteam focused dashboard. It displays network
+ *    tables data using a variety of widgets that can be positioned and controlled with robot code.
+ *   Helpful link: https://docs.wpilib.org/en/stable/docs/software/dashboards/shuffleboard/index.html
+
+ *   Use the variable(shuffleboardTab) to add it to your shuffleboard.
+ *   Example: shuffleboardTab.addBoolean("title of widget", Robot.arm::getPos());
+ *   There is many more functions that you can use, example addString, addNumber, etc.
+ */
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+import java.util.Map;
+
+public class UI {
+
+    public UI() {
+        ShuffleboardTab shuffleboardTab = Shuffleboard.getTab("FRC 2025");
+
+    }
+}
