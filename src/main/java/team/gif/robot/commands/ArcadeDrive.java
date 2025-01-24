3630a7e0e451775d2670c1ffacfbead0c3449c5d
rowan dyche <rowdyche09@gmail.com>
2025-01-23 19:49:50 -0600

    it might drive with tank drive

--- src/main/java/team/gif/robot/commands/ArcadeDrive.java
+++ src/main/java/team/gif/robot/commands/ArcadeDrive.java
@@ -1,27 +1,21 @@
 package team.gif.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import team.gif.robot.Robot;
 
-public class TankDrive extends Command {
+public class ArcadeDrive extends Command {
 
-    public TankDrive() {
+    public ArcadeDrive() {
         super();
-        addRequirements(Robot.driveTrain);
         //addRequirements(Robot.climber); // uncomment
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {}
+
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
-    public void execute() {
-        double rightPos = Robot.oi.driver.getRightY();
-        double leftPos = -Robot.oi.driver.getLeftY();
-
-        Robot.driveTrain.driveTank(leftPos,rightPos);
-    }
+    public void execute() {}
 
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
     @Override
@@ -31,7 +25,5 @@ public class TankDrive extends Command {
 
     // Called when the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-        Robot.driveTrain.driveTank(0,0);
-    }
+    public void end(boolean interrupted) {}
 }
