220b9d6148400fd96c370b40c9e03f62fc28d193
Eugene Kesey <eugenekesey3@gmail.com>
2025-02-01 14:37:09 -0600

    Timed Shooter

--- src/main/java/team/gif/robot/commands/HamBurgerShoot.java
+++ src/main/java/team/gif/robot/commands/HamBurgerShoot.java
@@ -5,21 +5,28 @@ import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 
 public class HamBurgerShoot extends Command {
-
+private int runs;
     public HamBurgerShoot() {
         super();
         addRequirements(Robot.hamBurger);
         addRequirements(Robot.hamBurgerRight);
+        addRequirements(Robot.hamBurgerIndexer);
         //addRequirements(Robot.climber); // uncomment
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {runs = 0;}
 
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
     public void execute() {
+        runs++;
+        double hamBurgerPercent = Constants.HAM_BURGER_TALON_PERCENT;
+        Robot.hamBurger.turnmotor(hamBurgerPercent);
+        if(runs>100){
+            Robot.hamBurgerIndexer.turnmotor(Constants.HAM_BURGER_INDEX_TALON_PERCENT);
+        }
         Robot.hamBurger.turnmotor(Constants.HAM_BURGER_TALON_PERCENT);
         Robot.hamBurgerRight.turnmotor(-Constants.HAM_BURGER_TALON_PERCENT);
     }
@@ -35,6 +42,7 @@ public class HamBurgerShoot extends Command {
     public void end(boolean interrupted) {
         Robot.hamBurger.turnmotor(0);
         Robot.hamBurgerRight.turnmotor(0);
+        Robot.hamBurgerIndexer.turnmotor(0);
 
     }
 }
