0396f0e181b6ad903899ac78be89a27f462718be
Eugene Kesey <eugenekesey3@gmail.com>
2025-02-27 18:48:26 -0600

    More un-commenting

--- src/main/java/team/gif/robot/RobotMap.java
+++ src/main/java/team/gif/robot/RobotMap.java
@@ -7,7 +7,7 @@ public abstract class RobotMap {
     public static final int DRIVER_CONTROLLER_ID = 0;
     public static final int AUX_CONTROLLER_ID = 1;
     public static final int TEST_CONTROLLER_ID = 2;
-    /**public static final int LEFT_FRONT_NEO = 50;
+    public static final int LEFT_FRONT_NEO = 50;
     public static final int LEFT_BACK_NEO = 16;
     public static final int RIGHT_BACK_NEO = 33;
     public static final int RIGHT_FRONT_NEO = 11;
@@ -17,7 +17,6 @@ public abstract class RobotMap {
     public static final int CORAL_DUMPER_NEO_TEST = 30;
     public static final int ALGAE_SHOOTER_NEO_INDEX2 = 59;
     public static final int PIGEON_ID = 5;
->>>>>>> Stashed changes
     public static final int TEST_SPARKMAX_ID = 1;
     public static final int TEST_TALON_ID = 3;
 }
--- src/main/java/team/gif/robot/commands/drivetrain/ArcadeDrive.java
+++ src/main/java/team/gif/robot/commands/drivetrain/ArcadeDrive.java
@@ -1,5 +1,5 @@
 package team.gif.robot.commands.drivetrain;
-/**
+
 import edu.wpi.first.wpilibj2.command.Command;
 import team.gif.robot.Robot;
 
