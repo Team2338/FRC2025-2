b65fc825c3fac59059c926e2c9399653ae6c328e
Eugene Kesey <eugenekesey3@gmail.com>
2025-03-18 18:32:17 -0500

    Arm code

--- src/main/java/team/gif/robot/commands/ArmDown.java
+++ src/main/java/team/gif/robot/commands/ArmDown.java
@@ -1,22 +1,26 @@
 package team.gif.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import team.gif.robot.Constants;
+import team.gif.robot.Robot;
 
 public class ArmDown extends Command {
 
     public ArmDown() {
         super();
-        //addRequirements(Robot.climber); // uncomment
+        addRequirements(Robot.arm);
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+    }
 
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
-    public void execute() {}
-
+    public void execute() {
+        Robot.arm.setPercent(-Constants.ARM_MOVE_PERCENT);
+    }
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
     @Override
     public boolean isFinished() {
@@ -25,5 +29,7 @@ public class ArmDown extends Command {
 
     // Called when the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        Robot.arm.setPercent(0.0);
+    }
 }
