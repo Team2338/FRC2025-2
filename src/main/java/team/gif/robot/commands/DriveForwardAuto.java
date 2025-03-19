c0d0783ffac0051af5f60ed56988d3e58409b99d
rowan dyche <rowdyche09@gmail.com>
2025-03-18 20:25:19 -0500

    finished command

--- src/main/java/team/gif/robot/commands/DriveForwardAuto.java
+++ src/main/java/team/gif/robot/commands/DriveForwardAuto.java
@@ -1,29 +1,50 @@
 package team.gif.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import team.gif.robot.Constants;
+import team.gif.robot.Robot;
+
+import static team.gif.robot.Robot.coralDumper;
 
 public class DriveForwardAuto extends Command {
-
+    public int time;
+    public int timer;
     public DriveForwardAuto() {
         super();
-        //addRequirements(Robot.climber); // uncomment
+        addRequirements(Robot.coralDumper,Robot.driveTrain); // uncomment
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {time = 0;
+        timer = 0;}
 
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
-    public void execute() {}
+    public void execute() {
+        if(time <= 5){
+            time += 1;
+            Robot.driveTrain.driveArcade(.8,0);
+        }
+        if(time > 100){
+            Robot.driveTrain.driveArcade(0,0);
+            timer+=1;
+            if(timer<25){coralDumper.turnmotor(Constants.CORAL_NEO_PERCENT);}
+            if(timer>=25){coralDumper.turnmotor(-Constants.CORAL_NEO_PERCENT);}
+        }
+    }
 
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
     @Override
     public boolean isFinished() {
-        return false;
+        if(timer>50){return true;}
+        else{return false;}
     }
 
     // Called when the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        Robot.coralDumper.turnmotor(0);
+
+    }
 }
