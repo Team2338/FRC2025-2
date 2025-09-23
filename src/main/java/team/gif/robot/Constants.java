04da2bab36302af8cb82bd9bee4e1c8494d0036c
Raul Perez <santiagoperezpr09@gmail.com>
2025-09-22 19:07:36 -0500

    constants

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -17,12 +17,12 @@ public final class Constants {
     public static final double CORAL_NEO_PERCENT_AUTOS = 1.0;
     public static final double ALGAE_SHOOTER_IN = 4;
     public static final double ALGAE_SHOOTER_NEO_VOLTAGE_FAR = 12;
-    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_CLOSE = 5;
-    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_PROCESSOR = 5;
-    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_INDEX = 9.3;
-    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_INDEX_FAR = 12;
+    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_CLOSE = 5.5;
+    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_PROCESSOR = 3.2;
+    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_INDEX = 6.75;
+    public static final double ALGAE_SHOOTER_NEO_VOLTAGE_INDEX_FAR = 9.5;
     public static final double ALGAE_SHOOTER_NEO_VOLTAGE_INDEX_PROCESSOR = 3;
-    public static final double CLOSE_SHOOT_RPM = 2000;
+    public static final double CLOSE_SHOOT_RPM = 2100;
     public static final double FAR_SHOOT_RPM = 4000;
     public static final double PROCESSOR_SHOOT_RPM = 1200;
     public static final double ARM_MOVE_VOLTAGE = 2;
@@ -33,7 +33,7 @@ public final class Constants {
     public static final double ARM_KG = 1.1703;
     public static final double ARM_KV = 6.1823;
     public static final double ARM_POSITION_TOLERANCE = 0.10;
-    public static final double ARM_COLLECT_POSITION =  6.700; //1.100; //6.750
+    public static final double ARM_COLLECT_POSITION =  6.45; //1.100; //6.750
     public static final double ARM_PROCESSOR_POSITION = 5.246; //0.835; //5.246
     public static final double ARM_FAR_SHOOT_POSITION = 1.225; //0.195; //1.225
     public static final double ARM_CLOSE_SHOOT_POSITION = 0.85; //0.120; //0.754
